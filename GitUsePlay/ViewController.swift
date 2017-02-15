fa8b4a8725afc30c3bda2c75f14b5ed1dc578564
Keith Russell <keithrussell@keiths-mbp.home>
2017-02-15 18:56:48 -0500

    Updated 1st commit on main branch

--- GitUsePlay/ViewController.swift
+++ GitUsePlay/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
     // Do any additional setup after loading the view, typically from a nib.
   }
 
- //REMOVED MEMORY WARNING
+ //1. MAIN BRANCH - REMOVED MEMORY WARNING
 
 
 }
